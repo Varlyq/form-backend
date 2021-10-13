12de8b3299af916c00e5a97d1b973b829a744ea2
aawezk456 <aawezk456@gmail.com>
2021-10-13 18:21:11 +0530

    Live Updated

--- app/db.js
+++ app/db.js
@@ -1,11 +1,20 @@
 'user strict';
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//     host     : "127.0.0.1",
+//     user     : "root",
+//     password : "",
+//     database : "real_estate",
+//     port     : 3306,
+//     multipleStatements: true,
+// });
+
 var connection = mysql.createConnection({
-    host     : "127.0.0.1",
-    user     : "root",
-    password : "",
-    database : "real_estate",
+    host     : process.env.DATABASE_HOST,
+    user     : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE_NAME,
     port     : 3306,
     multipleStatements: true,
 });
