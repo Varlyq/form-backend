d2f6962db176f87e5932f47481f3fbec90be92f6
aawezk456 <aawezk456@gmail.com>
2021-10-19 02:05:31 +0530

    Latest

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/api/realEstateform',
       // var query = `INSERT INTO customers (name,address) VALUES('aawez','[${newarrlabour}]')`;
       let requirements = JSON.stringify(req.body.requirements);
       console.log(requirements)
-      var query = `INSERT INTO project_reg (company_name,company_entity,reg_address,pan_no,gst_no,specialization,kharchi_credit_period,ra_bill_credit_period,project_name,project_type,project_address,site_no_storeys,project_current_status,level_completed,labour_camp_img,logistics_plan_site,additional_info,project_incharge_name,project_inch_mobile,project_inch_email,requirements,utr_no)
+      var query = `INSERT INTO real_estate (company_name,company_entity,reg_address,pan_no,gst_no,specialization,kharchi_credit_period,ra_bill_credit_period,project_name,project_type,project_address,site_no_storeys,project_current_status,level_completed,labour_camp_img,logistics_plan_site,additional_info,project_incharge_name,project_inch_mobile,project_inch_email,requirements,utr_no)
        VALUES ('${req.body.company_name}','${req.body.company_entity}','${req.body.reg_address}','${req.body.pan_no}','${req.body.gst_no}','${req.body.specialization}','${req.body.kharchi_credit_period}','${req.body.ra_bill_credit_period}','${req.body.project_name}','${req.body.project_type}','${req.body.project_address}',${req.body.site_no_storeys},'${req.body.project_current_status}','${req.body.level_completed}','[${newarrlabour}]','[${newarrlogistic}]','${req.body.additional_info}','${req.body.project_incharge_name}','${req.body.project_inch_mobile}','${req.body.project_inch_email}',${requirements},'${req.body.utr_no}')`;
       sql.query(query, function (err, result) {
         if (err) throw err;
